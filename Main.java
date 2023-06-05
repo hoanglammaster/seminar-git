03df0207afcea96233536b0f91c96934f85038b8
Hoang Lam <lam.hoangvan@apuscorp.com>
2023-06-05 09:44:04 +0700

    This is first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is first commit");
+        System.out.println("This is editing first commit");
+    }
+}
